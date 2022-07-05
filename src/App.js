9e01279bafd2a1c3b117d00ad46016c8a0e9da39
Amol Vekhande <apple@Apples-MacBook-Pro.local>
2022-07-06 00:45:22 +0530

    with homepage distinkt updated url34

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import Footer from './components/Footer';
 function App() {
   return (
    <>
-   <Router basename='/distinkt2'>
+   <Router>
     {/* <Navbar /> */}
     <Routes>
       <Route path='/' element={< HomePage />} />
